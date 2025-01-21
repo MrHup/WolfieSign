f23384c0c9d2eed13f642bebbef4519ab54db7ee
MrHup <hup.hubspace@gmail.com>
2025-01-21 13:34:40 +0200

    Initialize backend service for docusign integration

---
+++ backend/app/jwt_config.py
@@ -0,0 +1,8 @@
+DS_JWT = {
+    "ds_client_id": "8620b679-d833-4c96-a24f-5c1ecc1c448d",
+    "ds_impersonated_user_id": "3db5a41a-5e67-4ba8-8089-bfabd7d232e0",  # The id of the user.
+    "private_key_file": "./app/private.key", # Create a new file in your repo source folder named private.key then copy and paste your RSA private key there and save it.
+    "authorization_server": "account-d.docusign.com",
+    "doc_docx": "World_Wide_Corp_Battle_Plan_Trafalgar.docx",
+    "doc_pdf": "World_Wide_Corp_lorem.pdf"
+}
