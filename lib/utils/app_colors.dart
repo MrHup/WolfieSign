e2c4d86b74bb597dbb06802c73a8621eef24e35e
MrHup <hup.hubspace@gmail.com>
2025-01-21 16:13:54 +0200

    Update color scheme

--- lib/utils/app_colors.dart
+++ lib/utils/app_colors.dart
@@ -1,14 +1,11 @@
 import 'package:flutter/material.dart';
 
 class AppColors {
-  static const Color primaryColor = Color(0xFFF9286E);
-  static const Color secondaryColor = Color(0xFF3963F4);
-  static const Color backgroundColor = Color(0xFF0D0D0D);
-  static const Color backdropColor = Color(0xFF272727);
-  static const Color backdropPrimaryColor = Color.fromARGB(38, 248, 113, 210);
-  static const Color darkerBackdropColor = Color(0xFF1C1C1C);
+  static const Color primaryColor = Color(0xFF001D4B);
+  static const Color secondaryColor = Color(0xFF416AFF);
+  static const Color backgroundColor = Color(0xFFF7F7F7);
+  static const Color backdropColor = Color.fromARGB(123, 0, 0, 0);
 
-  static const Color primaryColorDarker = Color(0xFF631549);
   static const Color transparent = Color(0x00000000);
 
   static const Color fadedColor = Color.fromARGB(17, 255, 255, 255);
